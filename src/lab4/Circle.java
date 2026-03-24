0aa5ac8e0cad30ba94c8f7d17b0569d492643337
Mujtaba Amaan <mujtaba.amn@gmail.com>
2026-03-24 05:25:34 -0400

    Feature: add Circle class

---
+++ src/lab4/Circle.java
@@ -0,0 +1,19 @@
+package lab4;
+
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
